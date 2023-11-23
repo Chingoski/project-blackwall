f8e115d837c10250039eb81e843c22e8517d562c
Nikola <chingo@upshiftwork.com>
2023-11-23 22:20:32 +0100

    feat: Instaled Fractal package, created User transformer and added BodyGenerator

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
+use League\Fractal\TransformerAbstract;
 
 class BaseModel extends Model
 {
     use SoftDeletes;
+
+    public function getTransformer(?string $transformerClassName = null): TransformerAbstract|null
+    {
+        $modelClass = Str::afterLast(get_class($this), '\\');
+
+        $transformerClassName ??= "App\Transformers\\{$modelClass}Transformer";
+
+        if (class_exists($transformerClassName)) {
+            return new $transformerClassName();
+        }
+
+        return null;
+    }
 }
---
+++ app/Transformers/UserTransformer.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Transformers;
+
+use App\Models\User;
+use Carbon\Carbon;
+use League\Fractal\TransformerAbstract;
+
+class UserTransformer extends TransformerAbstract
+{
+    /**
+     * List of resources to automatically include
+     *
+     * @var array
+     */
+    protected array $defaultIncludes = [
+        //
+    ];
+
+    /**
+     * List of resources possible to include
+     *
+     * @var array
+     */
+    public array $availableIncludes = [
+        'city',
+    ];
+
+    /**
+     * A Fractal transformer.
+     *
+     * @param User $user
+     * @return array
+     */
+    public function transform(User $user): array
+    {
+        return [
+            'id'            => $user->getKey(),
+            'first_name'    => ucwords($user->first_name),
+            'last_name'     => ucwords($user->last_name),
+            'full_name'     => ucwords($user->full_name),
+            'email'         => $user->email,
+            'phone_number'  => $user->phone_number,
+            'address'       => $user->address,
+            'date_of_birth' => Carbon::parse($user->date_of_birth)->format('m/d/Y'),
+        ];
+    }
+}
