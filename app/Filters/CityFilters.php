b033cf6bc9543cc87742f226fb676420da45b356
Nikola <chingo@upshiftwork.com>
2024-01-03 13:31:11 +0100

    feat: Added order by name for cities

--- app/Filters/CityFilters.php
+++ app/Filters/CityFilters.php
@@ -4,6 +4,11 @@ namespace App\Filters;
 
 class CityFilters extends BaseFilters
 {
+    public function before(): void
+    {
+        $this->builder->orderBy('name');
+    }
+
     public function search(string $search): void
     {
         $this->builder->where('name', 'ilike', "%{$search}%");
